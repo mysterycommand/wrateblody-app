c40e713fae03d0e8c1d849e9a22970fd0068bcb8
Matt Hayes <matt@mysterycommand.com>
2013-09-22 22:50:57 -0500

    Sorting the sample data. Trying to get postion: sticky; to work.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,7 +7,7 @@
  */
 /** ================================================================================================================ **/
 
-/* jshint expr: true */
+/* jshint expr: true, bitwise: false */
 
 define([
 
@@ -36,9 +36,14 @@ define([
             $table = $('#js-table');
 
         data.posts.forEach(function(post) {
+            post.stars = Math.random() * 5 | 0;
             letter = post.title.charAt(0).toUpperCase();
             hash[letter] || (hash[letter] = []);
             hash[letter].push(post);
+        });
+
+        Object.keys(hash).forEach(function(key) {
+            $table.append(tbody({alpha: key, titles: hash[key]}));
         });
     };
 
