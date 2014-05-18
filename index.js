139ac9bd1e526f204cbba279e417851af30ad40e
Matt Hayes <matt@mysterycommand.com>
2014-05-18 14:01:34 -0700

    Remove Github base URL rewrite from index.html. Add modrewrite to index.js. Update deps.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 'use strict';
 
-const PORT = Number(process.env.PORT || 9000);
+var PORT = Number(process.env.PORT || 9000);
 
 var express = require('express');
+var modrewrite = require('connect-modrewrite');
 var logfmt = require('logfmt');
 
 var app = express();
 
 app.use(logfmt.requestLogger());
-app.use(express.static(__dirname, '/dist'));
+app.use(modrewrite(['^[^\\.]*$ /index.html [L]']));
+app.use(express.static(__dirname + '/dist'));
 
-app.listen(PORT);
+app.listen(PORT, function() {
+    console.log('Listening on port:', PORT);
+});
