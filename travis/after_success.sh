a985f1437c073a5e4b9911da30aae706c8a6e66c
Matt Hayes <matt@mysterycommand.com>
2014-05-26 19:28:06 -0700

    A different attempt at pushing to production branch after successful build, and then pushing to server after that.

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -2,16 +2,18 @@
 
 if [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
-  git clone https://${GH_OAUTH_TOKEN}@github.com/${GH_OWNER}/${GH_PROJECT_NAME} site > /dev/null 2>&1
-  cd site
-  if git checkout heroku; then git checkout --orphan heroku; fi
-  git rm -r .
-  cp -R ../dist/* .
+  git clone --branch=heroku https://${GH_OAUTH_TOKEN}@github.com/${GH_OWNER}/${GH_PROJECT_NAME} heroku > /dev/null
+
+  cd heroku
+  git rm -rf .
+  cp -Rf ../dist/* .
   cp ../dist/.* .
   git add -f .
+
   git config user.email "matt@mysterycommand.com"
   git config user.name "Matt Hayes"
+
   git commit -am "Travis build ${TRAVIS_BUILD_NUMBER}."
-  git push https://${GH_OAUTH_TOKEN}@github.com/${GH_OWNER}/${GH_PROJECT_NAME} HEAD:heroku > /dev/null 2>&1
+  git push -fq origin heroku > /dev/null 2>&1
 
 fi
