0660300c771536b6954feae430f0ae4a1d5564ad
Matt Hayes <matt@mysterycommand.com>
2014-05-26 23:43:54 -0700

    Refactor an abstract list view.

---
+++ test/static/spec/app/views/ListView.test.js
@@ -0,0 +1,22 @@
+/** ================================================================================================================ **/
+/**
+ * @fileOverview
+ *
+ * @author Matt Hayes <matt@mysterycommand.com>
+ * @version 0.1.2
+ */
+/** ================================================================================================================ **/
+
+/* jshint expr: true */
+
+'use strict';
+
+define(function(require) {
+    var ListView = require('app/views/ListView');
+
+    describe('ListView', function() {
+        it('should exist', function() {
+            expect(ListView).to.exist;
+        });
+    });
+});
